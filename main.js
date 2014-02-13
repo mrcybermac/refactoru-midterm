4edf1504fca6d62482433ec744c05c5a3b4f0c3c
MacGregor Wolstenholme <mrcybermac@gmail.com>
2014-02-13 13:00:00 -0700

    everything works, but trying to implement apply handlebars helper

--- main.js
+++ main.js
@@ -3,21 +3,29 @@ $(document).ready(function () {
     var priceData = {
         bitcoin: [700, 737, 738, 767, 783, 712, 711, 700, 738, 698, 678, 689, 675, 656, 660, 680, 685, 675, 665, 657, 675, 660, 675, 670, 660, 665, 655, 650],
         digitalcoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 15],
-        dogecoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 15],
-        feathercoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 15],
+        dogecoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 12],
+        feathercoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 18],
         litecoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 15],
-        megacoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 15],
-        mooncoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 1500],
-        namecoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 15],
-        novacoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 15],
-        nxt: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 150],
-        peercoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 150]
+        megacoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 14],
+        mooncoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 56],
+        namecoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 12],
+        novacoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 13],
+        nxt: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 65],
+        peercoin: [1, 4, 4, 7, 5, 9, 10, 8, 4, 5, 7, 5, 9, 10, 7, 4, 6, 7, 5, 9, 10, 6, 4, 4, 7, 5, 9, 17]
     }
 
 //////////////////////////////  HANDLEBARS HELPERS  ///////////////////////////////////
 
     Handlebars.registerHelper('toLowerCase', function (str) {
         return str.toLowerCase();
+    });
+
+    Handlebars.registerHelper('apply', function (fnc, obj) {
+        console.log(arguments);
+        console.log(Array.prototype.slice.call(Array.prototype.slice.call(arguments)));
+        //console.log(Array.prototype.shift.call(arguments));
+        //console.log(Array.prototype.slice.call(arguments));
+        return fnc.apply(obj, Array.prototype.slice.call(Array.prototype.slice.call(arguments)));
     });
 
     Handlebars.registerHelper('createTextElement', function (str, obj) {
@@ -31,29 +39,33 @@ $(document).ready(function () {
     var HandlebarsHTMLTrunkCreatable = function (scriptTagID) {
         this.scriptTagID = scriptTagID;
     };
-    HandlebarsHTMLTrunkCreatable.prototype.createJqueryElement = function () {
-        var source = $(this.scriptTagID).html();  // make this object property?
+    HandlebarsHTMLTrunkCreatable.prototype.createJqueryElement = function (scriptTagID) {
+        //console.log("hello",scriptTagID,arguments);
+        var source = $(scriptTagID?scriptTagID:this.scriptTagID).html();  // make this object property?
         var template = Handlebars.compile(source);
         return $(template(this));
     };
-    HandlebarsHTMLTrunkCreatable.prototype.createDOMElement = function () {
-        return this.createJqueryElement()[0];
+    HandlebarsHTMLTrunkCreatable.prototype.createDOMElement = function (scriptTagID) {
+        return this.createJqueryElement(scriptTagID)[0];
     };
-    HandlebarsHTMLTrunkCreatable.prototype.createTextElement = function () {
-        return this.createJqueryElement()[0].outerHTML;
+    HandlebarsHTMLTrunkCreatable.prototype.createTextElement = function (scriptTagID) {
+        //console.log("hello",scriptTagID,arguments);
+        return this.createJqueryElement(scriptTagID)[0].outerHTML;
     };
 
 ////////////////
 
     var Crypto = function (title, imageName) {
+        HandlebarsHTMLTrunkCreatable.call(this);
         this.title = title;
         this.imageName = imageName;
     };
+    Crypto.prototype = new HandlebarsHTMLTrunkCreatable();
     Crypto.prototype.getPriceData = function () {
         return priceData[this.title.toLowerCase()];
     };
     Crypto.prototype.getCurrentPrice = function () {
-        return 700;
+        return _.last(priceData[this.title.toLowerCase()]);
     };
     Crypto.prototype.createSparkline = function () {
         //see http://omnipotent.net/jquery.sparkline/#s-faq
@@ -66,7 +78,7 @@ $(document).ready(function () {
 
         var that = this;
         setTimeout(function () {
-            $("#" + that.title.toLowerCase() + "-sparkline").sparkline(that.getPriceData());
+            $("." + that.title.toLowerCase() + "-sparkline").sparkline(that.getPriceData());
         }, 0);
 
     };
@@ -76,17 +88,36 @@ $(document).ready(function () {
     var UserSettings = function (scriptTagID) {
         HandlebarsHTMLTrunkCreatable.call(this, scriptTagID);
         this.settings = {};
-        for (var i = 0; i < cryptoArray.length; i++) {
-            this.settings[cryptoArray[i].title] = 0;
+        for (var key in cryptoObject){
+            this.settings[key] = {
+                cryptoObj: cryptoObject[key],
+                quantity: 0
+            }
         }
     };
     UserSettings.prototype = new HandlebarsHTMLTrunkCreatable();
+    UserSettings.prototype.addCrypto = function (crypto) {
+        if (!this.settings.hasOwnProperty(crypto.toLowerCase())){
+            this.settings[crypto.toLowerCase()] = {
+                cryptoObj: cryptoObject[crypto.toLowerCase()],
+                quantity: 0
+            }
+        }
+    };
+    UserSettings.prototype.removeCrypto = function (crypto) {
+        if (this.settings.hasOwnProperty(crypto.toLowerCase())){
+            delete this.settings[crypto.toLowerCase()];
+        }
+    };
+    UserSettings.prototype.updateQuantity = function (crypto, quantity) {
+
+    };
 
 ////////////////
 
     var Portfolio = function (cryptos, scriptTagID) {
         HandlebarsHTMLTrunkCreatable.call(this, scriptTagID);
-        this.cryptos = cryptos; //array of Crypto objects
+        this.cryptos = cryptos; //object of Crypto objects
     }
     Portfolio.prototype = new HandlebarsHTMLTrunkCreatable();
 
@@ -95,37 +126,61 @@ $(document).ready(function () {
 
 
 //////////////////////////////  INSTANTIATE OBJECTS  ///////////////////////////////////
-    var bitcoin = new Crypto("Bitcoin", "bitcoin.png");
-    var digitalcoin = new Crypto("Digitalcoin", "digitalcoin.png");
-    var dogecoin = new Crypto("Dogecoin", "dogecoin.png");
-    var feathercoin = new Crypto("Feathercoin", "feathercoin.png");
-    var litecoin = new Crypto("Litecoin", "litecoin.jpg");
-    var megacoin = new Crypto("Megacoin", "megacoin.png");
-    var mooncoin = new Crypto("Mooncoin", "mooncoin.png");
-    var namecoin = new Crypto("Namecoin", "namecoin.png");
-    var novacoin = new Crypto("Novacoin", "novacoin.png");
-    var nxt = new Crypto("Nxt", "nxt.jpg");
-    var peercoin = new Crypto("Peercoin", "peercoin.png");
+//    var bitcoin = new Crypto("Bitcoin", "bitcoin.png");
+//    var digitalcoin = new Crypto("Digitalcoin", "digitalcoin.png");
+//    var dogecoin = new Crypto("Dogecoin", "dogecoin.png");
+//    var feathercoin = new Crypto("Feathercoin", "feathercoin.png");
+//    var litecoin = new Crypto("Litecoin", "litecoin.jpg");
+//    var megacoin = new Crypto("Megacoin", "megacoin.png");
+//    var mooncoin = new Crypto("Mooncoin", "mooncoin.png");
+//    var namecoin = new Crypto("Namecoin", "namecoin.png");
+//    var novacoin = new Crypto("Novacoin", "novacoin.png");
+//    var nxt = new Crypto("Nxt", "nxt.jpg");
+//    var peercoin = new Crypto("Peercoin", "peercoin.png");
 
-    var cryptoArray = [];  //global array of all defined cryptos
+//    var cryptoArray = [];  //local array of all defined cryptos
+//    cryptoArray.push(
+//        bitcoin,
+//        digitalcoin,
+//        dogecoin,
+//        feathercoin,
+//        litecoin,
+//        megacoin,
+//        mooncoin,
+//        namecoin,
+//        novacoin,
+//        nxt,
+//        peercoin);
 
-    cryptoArray.push(
-        bitcoin,
-        digitalcoin,
-        dogecoin,
-        feathercoin,
-        litecoin,
-        megacoin,
-        mooncoin,
-        namecoin,
-        novacoin,
-        nxt,
-        peercoin);
 
-    var portfolio = new Portfolio(cryptoArray, "#portfolio-template");
+    //cryptoArray is created solely for the purpose of subsequently building cryptoObject.
+    //cryptoObject is used throughout the app to access the Crypto objects:
+
+    var cryptoArray = [  //local array of all defined cryptos
+    new Crypto("Bitcoin", "bitcoin.png"),
+    new Crypto("Digitalcoin", "digitalcoin.png"),
+    new Crypto("Dogecoin", "dogecoin.png"),
+    new Crypto("Feathercoin", "feathercoin.png"),
+    new Crypto("Litecoin", "litecoin.jpg"),
+    new Crypto("Megacoin", "megacoin.png"),
+    new Crypto("Mooncoin", "mooncoin.png"),
+    new Crypto("Namecoin", "namecoin.png"),
+    new Crypto("Novacoin", "novacoin.png"),
+    new Crypto("Nxt", "nxt.jpg"),
+    new Crypto("Peercoin", "peercoin.png")
+    ];
+
+    var cryptoObject = {};  //local object containing all crypto objects by title key
+    for (var i = 0; i < cryptoArray.length; i++) {
+        cryptoObject[cryptoArray[i].title.toLowerCase()] = cryptoArray[i];
+    }
+
+
+
+    var portfolio = new Portfolio(cryptoObject, "#portfolio-template");
 
     var userSettings = new UserSettings("#settings-template");
-    //console.log(userSettings);
+        //console.log(userSettings);
 
 
 //////////////////////////////  MAIN  ///////////////////////////////////
@@ -139,7 +194,9 @@ $(document).ready(function () {
 
 
 //TODO
+//About modal
 //settings rendered and saved
 //total value calculated and for each
 //html storage of settings... set userSettings=html storage object if exist
 //sort by alpha, price, portfolio value
+//animate stuff
